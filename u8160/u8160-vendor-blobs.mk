55a5c3543dac284c375bc6911bdb15d6afda61f2
Alberto96 <albertoguanti@gmail.com>
2014-07-18 14:57:40 +0200

    u8160: clean-up proprietaries

--- u8160/u8160-vendor-blobs.mk
+++ u8160/u8160-vendor-blobs.mk
@@ -15,22 +15,12 @@
 # All the blobs necessary for u8160
 PRODUCT_COPY_FILES += \
     vendor/huawei/u8160/proprietary/etc/AudioFilterU8160.csv:system/etc/AudioFilterU8160.csv \
-    vendor/huawei/u8160/proprietary/etc/AudioFilterU8160.csv:obj/etc/AudioFilterU8160.csv \
     vendor/huawei/u8160/proprietary/etc/bluetooth/BCM4329.hcd:system/etc/bluetooth/BCM4329.hcd \
-    vendor/huawei/u8160/proprietary/etc/bluetooth/BCM4329.hcd:obj/etc/bluetooth/BCM4329.hcd \
     vendor/huawei/u8160/proprietary/bin/akmd2:system/bin/akmd2 \
-    vendor/huawei/u8160/proprietary/bin/akmd2:obj/bin/akmd2 \
     vendor/huawei/u8160/proprietary/bin/akmd8962:system/bin/akmd8962 \
-    vendor/huawei/u8160/proprietary/bin/akmd8962:obj/bin/akmd8962 \
     vendor/huawei/u8160/proprietary/bin/akmd8975:system/bin/akmd8975 \
-    vendor/huawei/u8160/proprietary/bin/akmd8975:obj/bin/akmd8975 \
     vendor/huawei/u8160/proprietary/lib/hw/sensors.u8160.so:system/lib/hw/sensors.u8160.so \
-    vendor/huawei/u8160/proprietary/lib/hw/sensors.u8160.so:obj/lib/hw/sensors.u8160.so \
     vendor/huawei/u8160/proprietary/wifi/firmware.bin:system/wifi/firmware.bin \
-    vendor/huawei/u8160/proprietary/wifi/firmware.bin:obj/wifi/firmware.bin \
     vendor/huawei/u8160/proprietary/wifi/firmware_apsta.bin:system/wifi/firmware_apsta.bin \
-    vendor/huawei/u8160/proprietary/wifi/firmware_apsta.bin:obj/wifi/firmware_apsta.bin \
     vendor/huawei/u8160/proprietary/wifi/firmware_test.bin:system/wifi/firmware_test.bin \
-    vendor/huawei/u8160/proprietary/wifi/firmware_test.bin:obj/wifi/firmware_test.bin \
-    vendor/huawei/u8160/proprietary/wifi/nvram.txt:system/wifi/nvram.txt \
-    vendor/huawei/u8160/proprietary/wifi/nvram.txt:obj/wifi/nvram.txt
+    vendor/huawei/u8160/proprietary/wifi/nvram.txt:system/wifi/nvram.txt
